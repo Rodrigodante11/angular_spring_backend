6d0459774c0627cf889e13cbed340cc7bc1a834c
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-08-10 17:50:27 -0300

    config Oauth para utilizar token JWT

--- src/main/java/io/github/rodrigodante/clientes/config/AuthorizationServerConfig.java
+++ src/main/java/io/github/rodrigodante/clientes/config/AuthorizationServerConfig.java
@@ -2,6 +2,7 @@ package io.github.rodrigodante.clientes.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,6 +12,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -19,15 +22,27 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Value("{$secutiry.jwt.signing-key}")
+    private String signingKey;
+
     @Bean
     public TokenStore tokenStore(){
-        return new InMemoryTokenStore();
+
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        tokenConverter.setSigningKey(signingKey);
+        return tokenConverter;
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .tokenStore(tokenStore())  // de onde vai vi os tokens
+                .accessTokenConverter(accessTokenConverter())
                 .authenticationManager(authenticationManager);
     }
 
